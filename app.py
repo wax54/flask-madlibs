902f15791e8a864609b3f426ea7ab5e7210154b4
sam <wax654@gmail.com>
2021-02-12 14:57:18 -0800

    added homepage

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template as render
 from stories import Story, story_list
 
 app = Flask(__name__)
+@app.route('/')
+def pick_a_story():
+    return render('pick_a_story.html',story_list=story_list)
 
 @app.route('/story/<int:story_id>')
 def story_form(story_id):
     story = story_list[story_id]
-    return render_template('story_form.html', prompts=story.prompts, story_id = story_id)
+    return render('story_form.html', prompts=story.prompts, story_id = story_id)
 
 @app.route('/story_result/<int:story_id>', methods=["GET"])
 def story_result(story_id):
@@ -17,4 +20,4 @@ def story_result(story_id):
     ans = {prompt:answer for (prompt, answer) in request.args.items()}
     result = story.generate(ans).upper()
     print(result, story)
-    return render_template('story_result.html', story=result)
+    return render('story_result.html', story=result)
--- stories.py
+++ stories.py
@@ -11,18 +11,19 @@ class Story:
         ...     "I love to {verb} a good {noun}.")
 
     To generate text from a story, pass in a dictionary-like thing
-    of {prompt: answer, promp:answer):
+    of {prompt: answer, promp:answer}:
 
         >>> ans = {"verb": "eat", "noun": "mango"}
         >>> s.generate(ans)
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, words, text, description=None):
         """Create story with words and template text."""
 
         self.prompts = words
         self.template = text
+        self.description = description if description else text
 
     def generate(self, answers):
         """Substitute answers into text."""
@@ -40,10 +41,12 @@ class Story:
 
 story_list = [
     Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
+        ["place", "noun", "verb", "adjective", "plural_noun"],
+        """Once upon a time in a long-ago {place}, there lived a
     large {adjective} {noun}. It loved to {verb} {plural_noun}."""),
+
     Story(
-    ["noun"],
-    """A nice Succulent {noun} is the best thing in the world.""")      
+        ["noun"],
+        """A nice Succulent {noun} is the best thing in the world.""",
+        description="A Thrilling Tale of a joy")
 ]
