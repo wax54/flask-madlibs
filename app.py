ff077a7355c9b555cc70a59097fa0e1b1c84d62a
sam <wax654@gmail.com>
2021-02-12 14:41:51 -0800

    Lookin nice

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ def story_form(story_id):
     story = story_list[story_id]
     return render_template('story_form.html', prompts=story.prompts, story_id = story_id)
 
-@app.route('/story_result/<int:story_id>',methods=["POST"])
+@app.route('/story_result/<int:story_id>', methods=["GET"])
 def story_result(story_id):
-    if()
+    if not len(request.args):
+        return story_form(story_id)
     story = story_list[story_id]
     #turn the array of tuples into a dict for Story.generate()
-    ans = {prompt:answer for (prompt, answer) in request.form.items()}
-    result = story.generate(ans)
-    print(result,story)
+    ans = {prompt:answer for (prompt, answer) in request.args.items()}
+    result = story.generate(ans).upper()
+    print(result, story)
     return render_template('story_result.html', story=result)
